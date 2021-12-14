186d9a1ef62713fa3a8888c22f72eb529aabc8ca
Jeffrey Hu <zhiwehu@gmail.com>
2021-12-14 17:02:46 +0800

    default code text

--- src/reducers/codeSlice.js
+++ src/reducers/codeSlice.js
@@ -1,7 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
+let demoCode = `from turtle import *
+from math import *
+from random import *
 
+for i in range(10):
+    print("#{}. Hello World".format(i))
+
+speed(0)
+seth(90)
+colormode(255)
+up()
+goto(0, -200)
+down()
+
+def square(s, a, n):
+    if n==0:
+        return
+    down()
+    color(randint(0, 255), randint(0, 255), randint(0, 255))
+    begin_fill()
+    for i in range(4):
+        fd(s)
+        rt(90)
+    end_fill()    
+    up()
+    fd(s)
+    lt(a)
+    
+    square(s*cos(radians(a)), a, n-1)
+        
+    rt(90)
+    fd(s*cos(radians(a)))
+    
+    square(s*sin(radians(a)), a, n-1)
+    
+    bk(s*cos(radians(a)))
+    lt(90-a)
+    bk(s) 
+
+square(100, 36, 5)`;
 const initialState = {
-  code: "",
+  code: demoCode,
   result: "",
 };
 
