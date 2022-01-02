644d0848d5bd9c94bdbb0f6a62c57ce89764a0ba
Jeffrey Hu <zhiwehu@gmail.com>
2022-01-02 10:15:51 +0800

    update demo

--- src/reducers/pythonFileListSlice.js
+++ src/reducers/pythonFileListSlice.js
@@ -1,20 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
-export const demoCode = `from turtle import *
-color('darkblue')
-ht()
+export const demoCode = `import math
+from datetime import date, timedelta
+from turtle import *
+
+y, m, d = input("请输入你的出生日期(格式：年-月-日)：").split("-")
+years = int(input("请输入你希望活多少岁："))
+birthday = date(year=int(y), month=int(m), day=int(d))
+today = date.today()
+deathday = birthday + timedelta(days=math.ceil(years * 365.25))
+
+used = today - birthday
+left = deathday - today
+
+#setup(800, 800)
+shape("square")
+#shapesize(0.35, 0.35)
+speed(0)
 up()
-goto(0, -100)
-down()
-circle(100)
-up()
-goto(-60, 50)
-dot(30)
-goto(60, 50)
-dot(30)
-goto(-40, 0)
-down()
-rt(60)
-circle(50, 120)`;
+title = "你已经过了{}天，还剩下{}天。".format(used.days, left.days)
+goto(0, 200)
+write(title, align="center", font=("Kai", 20, "bold"))
+goto(0, 180)
+
+r = 5
+total_weeks = int(used.days / 7 + left.days / 7)
+dots = math.ceil(math.sqrt(total_weeks))
+goto(-dots / 2 * r, dots / 2 * r)
+line = 0
+color("orange")
+for i in range(total_weeks):
+    if i == used.days // 7:
+        color("lightgreen")
+    if i % dots == 0:
+        x = -dots // 2 * r
+        y = dots // 2 * r - line * r
+        line = line + 1
+        goto(x, y)
+    stamp()
+    fd(r)
+`;
 
 export const getCurrentDateTime = () => {
   const dt = new Date();
